14e921703813eb4805cc9f4c9b31312542eb03fa
Najuna James <seanjems@gmail.com>
2022-06-04 07:44:38 +0300

    Added posts cards

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import PostsCard from "../PostsCard/PostsCard";
 import PostShare from "../PostShare/PostShare";
 import "./PostSide.css";
 const PostSide = () => {
   return (
     <div className="PostSide">
       <PostShare />
+      <PostsCard />
     </div>
   );
 };
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Posts.css";
+
+import Like from "../../img/notlike.png";
+import Liked from "../../img/like.png";
+import Share from "../../img/share.png";
+import Comment from "../../img/comment.png";
+
+const Posts = ({ data }) => {
+  return (
+    <div className="Posts">
+      <span>
+        <b>{data.name}</b>
+      </span>
+      {data.desc && <span>{data.desc}</span>}
+      {data.img && <img src={data.img} alt="" />}
+      <div className="shareOptions">
+        <img src={data.liked ? Liked : Like} alt="" />
+        <img src={Comment} alt="" />
+        <img src={Share} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
