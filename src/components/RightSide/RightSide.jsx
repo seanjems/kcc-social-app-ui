09b9350d8f08412b7c7bca6b4eee9883bfc198b5
Najuna James <seanjems@gmail.com>
2022-06-04 08:42:14 +0300

    Added right side components on home

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Home from "../../img/home.png";
+import Notification from "../../img/noti.png";
+import Messages from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
+
+import "./RightSide.css";
+import TrendCard from "../TrendCard/TrendCard";
+
+const RightSide = () => {
+  return (
+    <div className="RightSide">
+      <div className="navIcons">
+        <img src={Home} alt="" />
+        <UilSetting />
+        <img src={Notification} alt="" />
+        <img src={Messages} alt="" />
+      </div>
+      <TrendCard />
+      <button className="button sh-button">Share</button>
+    </div>
+  );
+};
+
+export default RightSide;
---
+++ src/components/TrendCard/TrendCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { TrendData } from "../../Data/TrendData";
+
+import "./TrendCard.css";
+
+const TrendCard = () => {
+  return (
+    <div className="TrendCard">
+      <h3>Trending topics</h3>
+      {TrendData.map((trend) => (
+        <div>
+          <span>#{trend.name}</span>
+          <span>{trend.shares}k shares</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TrendCard;
